eec519487df961685a5b31d285ce3e2f62c12ff2
LukaszCiaston-coder <lukasz.ciaston@interia.pl>
2024-10-08 21:00:03 +0200

    burger menu

--- js/mobile.js
+++ js/mobile.js
@@ -2,12 +2,21 @@ function toggleNavbar() {
     const navbarToggle = document.querySelector('.navbar-toggle');
     const mobileMenu = document.querySelector('.mobile-menu');
     const toggleSpans = navbarToggle.querySelectorAll('span');
+    const mobileMenuLinks = document.querySelectorAll('.mobile-menu__link');
 
     navbarToggle.addEventListener('click', function() {
         navbarToggle.classList.toggle('active');
         mobileMenu.classList.toggle('active');
-        
         toggleSpans[0].classList.toggle('hidden');
+    });
+
+    // Dodaj event listener do każdego linku w mobilnym menu
+    mobileMenuLinks.forEach(link => {
+        link.addEventListener('click', function() {
+            navbarToggle.classList.remove('active');
+            mobileMenu.classList.remove('active');
+            toggleSpans[0].classList.remove('hidden');
+        });
     });
 }
 
@@ -16,14 +25,17 @@ function handleResize() {
     const mobileMenu = document.querySelector('.mobile-menu');
 
     if (window.innerWidth < 768) {
-        navbarToggle.style.display = 'block';
+        navbarToggle.style.display = 'block'; // Pokaż hamburger
         mobileMenu.classList.remove('active');
     } else {
-        navbarToggle.style.display = 'none';
+        navbarToggle.style.display = 'none'; // Ukryj hamburger w wersji desktopowej
         mobileMenu.classList.remove('active');
     }
 }
 
 window.addEventListener('resize', handleResize);
-window.addEventListener('DOMContentLoaded', handleResize);
-window.addEventListener('DOMContentLoaded', toggleNavbar);
+window.addEventListener('DOMContentLoaded', () => {
+    handleResize();
+    toggleNavbar();
+});
+
